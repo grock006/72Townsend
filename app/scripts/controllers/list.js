980cf3c599bf0912408190074316bea9585e30a8
Greg Rock <grock006@gmail.com>
2015-08-27 12:02:48 -0700

    replaced list select buttons with dropdowns

--- app/scripts/controllers/list.js
+++ app/scripts/controllers/list.js
@@ -5,8 +5,33 @@ angular.module('72TownsendApp')
 
   	//init setting for 'sort by'
   	$scope.sort = "floor";
+    $scope.sortText = "floor"
   	$scope.listHidden = false;
   	$scope.floorHidden = false;
+
+     $scope.selectListBedroom = function selectListBedroom(num){
+        if(Number.isInteger(num)){
+            var number = num.toString();
+            $rootScope.bedrooms = number;
+            $scope.bedroomText = '';
+        }
+        else{
+            $rootScope.bedrooms = '';
+            $scope.bedroomText = 'ALL';
+        }
+
+    }
+
+    $scope.selectSort = function selectSort(str){
+        if(str === 'interiorSqFt'){
+            $scope.sort = 'interiorSqFt';
+            $scope.sortText = 'Square Footage';
+        }
+        else{
+            $scope.sort = str;
+            $scope.sortText = str;
+        }
+    }
 
     $scope.slides = [];
 
